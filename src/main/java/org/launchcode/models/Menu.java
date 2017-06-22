3afbdc4045d44ff97a2a02a2190a1ddfd8360b64
AndrewScalise <paradigmcomplex@gmail.com>
2017-06-22 15:13:53 -0500

    Finished but not working

--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -1,9 +1,11 @@
 package org.launchcode.models;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,13 +23,11 @@ public class Menu {
     private String name;
 
     @ManyToMany
-    private List<Cheese> cheeses = new ArrayList<>();
+    private List<Cheese> cheeses;
 
     public Menu() {}
 
-    public Menu(String name){
-        this.name = name;
-    }
+    public void addItem(Cheese item) {cheeses.add(item);}
 
     public int getId() {
         return id;
@@ -43,9 +43,5 @@ public class Menu {
 
     public List<Cheese> getCheeses() {
         return cheeses;
-    }
-
-    public void setCheeses(List<Cheese> cheeses) {
-        this.cheeses = cheeses;
     }
 }
